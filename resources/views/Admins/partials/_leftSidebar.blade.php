58c613ead8a149b6a45cfa3421963ed3c93e87f6
rhpream <radoanhossaiinpream@outlook.com>
2018-02-23 01:56:34 +0600

    Removed user info

--- resources/views/Admins/partials/_leftSidebar.blade.php
+++ resources/views/Admins/partials/_leftSidebar.blade.php
@@ -4,15 +4,15 @@
     <!-- sidebar: style can be found in sidebar.less -->
     <section class="sidebar">
         <!-- Sidebar user panel -->
-        <div class="user-panel">
-            <div class="pull-left image">
-                <img src="{{asset('img')}}/{{Auth::user()->profile_pic}}" class="img-circle" alt="User Image">
-            </div>
-            <div class="pull-left info">
-                <p>{{ Auth::user()->name }}</p>
-                <a href="#"><i class="fa fa-circle text-success"></i> Online</a>
-            </div>
-        </div>
+        {{--<div class="user-panel">--}}
+            {{--<div class="pull-left image">--}}
+                {{--<img src="{{asset('img')}}/{{Auth::user()->profile_pic}}" class="img-circle" alt="User Image">--}}
+            {{--</div>--}}
+            {{--<div class="pull-left info">--}}
+                {{--<p>{{ Auth::user()->name }}</p>--}}
+                {{--<a href="#"><i class="fa fa-circle text-success"></i> Online</a>--}}
+            {{--</div>--}}
+        {{--</div>--}}
         <!-- search form -->
         <form action="#" method="get" class="sidebar-form">
             <div class="input-group">
